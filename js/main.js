f554d5008cebf13573af7d16d7e4809cc2a2483a
m <1204.espresso@gmail.com>
2026-05-13 22:37:06 +0900

    アニメーション追加

--- js/main.js
+++ js/main.js
@@ -18,7 +18,22 @@ $(window).scroll(function () {
         $(this).addClass("slide");
       } 
     });
-
+  
+        // concept-textクラスに対して順に処理を行う
+    $(".fade-up").each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // concept-textクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 100) {
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
   });
 
-});
+  });
