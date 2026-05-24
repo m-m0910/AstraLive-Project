28e86f764584cf54853d101e9fb3850263b8f6b9
m <1204.espresso@gmail.com>
2026-05-25 01:55:39 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -34,6 +34,21 @@ $(function () {
         $(this).css("transform", "translateY(0)");
       }
     });
+
+      $(".slide-left").each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // concept-textクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 100) {
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateX(0)");
+      }
+    });
   });
 
 $(window).on("scroll load",function () {    // concept-textクラスに対して順に処理を行う
@@ -60,5 +75,66 @@ $(".count-text") .on("input",function(){
 $(this)
       .siblings(".count")
       .text(textLength + "/500字");
+
+});
+
+$(function () {
+
+  $(".btn").on("click", function () {
+
+    localStorage.setItem(
+      "realName",
+      $("#real-name").val()
+    );
+
+    localStorage.setItem(
+      "realNameKana",
+      $("#real-name-kana").val()
+    );
+
+    localStorage.setItem(
+      "age",
+      $("#age").val()
+    );
+
+    localStorage.setItem(
+      "mail",
+      $("#mail").val()
+    );
+
+    localStorage.setItem(
+      "number",
+      $("#number").val()
+    );
+
+    localStorage.setItem(
+      "reason",
+      $("#reason").val()
+    );
+
+    localStorage.setItem(
+      "game",
+      $("#game").val()
+    );
+
+    localStorage.setItem(
+      "plan",
+      $("#plan").val()
+    );
+
+    localStorage.setItem(
+      "appeal",
+      $("#appeal").val()
+    );
+
+    localStorage.setItem(
+      "gender",
+      $('input[name="gender"]:checked')
+        .parent()
+        .text()
+        .trim()
+    );
+
+  });
 
 });
